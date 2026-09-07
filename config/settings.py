764fcb1f8db3170f977f03a1ba9e95a0da3080db
karthikaram06 <karthikasmr2006@gmail.com>
2026-09-07 22:08:04 +0530

    Fix CSS static loading

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,16 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # --------------------------------------------------
 # MEDIA FILES
